ff39403b228fe6c2a190a7b6f17e0cdc0cabfb66
mitoop <me@mitoop.com>
2025-03-05 09:11:21 +0800

    tron

--- src/Wallets/TronWallet.php
+++ src/Wallets/TronWallet.php
@@ -2,9 +2,41 @@
 
 namespace Mitoop\XCrypto\Wallets;
 
+use Elliptic\EC;
+use kornrunner\Keccak;
+use StephenHill\Base58;
+
 class TronWallet implements WalletInterface
 {
-    public function generate(): Wallet {}
+    public function generate(): Wallet
+    {
+        $ec = new EC('secp256k1');
+        $key = $ec->genKeyPair();
+
+        $privateKey = $key->getPrivate('hex');
+        $publicKey = $key->getPublic('hex');
+
+        $hexAddress = $this->generateHexAddress($publicKey);
+        $address = $this->convertHexToBase58($hexAddress);
+
+        return new Wallet($address, $privateKey, $publicKey, $hexAddress);
+    }
+
+    protected function generateHexAddress(string $publicKey): string
+    {
+        $hash = Keccak::hash(substr(hex2bin($publicKey), -64), 256);
+
+        return '41'.substr($hash, 24);
+    }
+
+    protected function convertHexToBase58(string $hexString): string
+    {
+        $address = hex2bin($hexString);
+        $checksum = substr(hash('sha256', hex2bin(hash('sha256', $address))), 0, 8);
+        $address .= hex2bin($checksum);
+
+        return (new Base58)->encode($address);
+    }
 
     public function validate(string $address): bool {}
 }
